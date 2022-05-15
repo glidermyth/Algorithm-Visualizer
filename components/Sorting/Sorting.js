36fa4c26fda08358edd2b02c844597ae5c4ede25
Glider Myth <glidermyth@gmail.com>
2022-05-15 12:34:44 +0530

    Implementing Bubble Sort algorithm for sorting component.

--- components/Sorting/Sorting.js
+++ components/Sorting/Sorting.js
@@ -4,16 +4,25 @@
 const input_field = document.querySelector("#num-array");
 const input_btn = document.querySelector(".input-btn");
 const canvas = document.querySelector(".canvas");
+const bubbleSort = document.querySelector(".bubbleSort");
 
 //  variables and arrays
 
-const numArray = [];
+let numArray = [];
+const minHeight = 30;
+const minElemHeight = 12;
 
 
 //  function to change input string to array of integers
 
-function stringToArray()
+function stringToNumArray()
 {
+    numArray = [];
+    let canvasElementCount = canvas.childElementCount;
+    for(let i=0;i<canvasElementCount;i++)
+    {
+        canvas.firstElementChild.remove();
+    }
     let element = "";
     let inputString = input_field.value;
     for (let i = 0; i < inputString.length; i++) {
@@ -37,8 +46,11 @@ function createSpan()
     numArray.forEach((value)=>{
         let span = document.createElement("span");
         span.classList.add("canvas-span-element");
-        span.style.height = value+"px";
-        span.style.width = value+"px";
+        span.textContent = value;
+        if(value > minElemHeight)
+        {
+            span.style.height = (minHeight+(value-minElemHeight))+"px";
+        }
         canvas.append(span);
     });
 }
@@ -46,7 +58,59 @@ function createSpan()
 //  Adding click event to input_btn
 
 input_btn.addEventListener("click",()=>{
-    stringToArray();
+    stringToNumArray();
     // console.log(numArray);
     createSpan();
+});
+
+
+//  Implementing bubble sort algorithm and adding click event to the button
+bubbleSort.addEventListener("click",(event)=>{
+    let canvasChildren = canvas.children;
+    // console.dir(canvasChildren);
+    let counter = 1;
+    let time = 0;
+    let spanColor = "#7900FF";
+    let spanCmpColor = "#FF6363";
+    while(counter<canvasChildren.length)
+    {
+        for(let i =0;i<canvasChildren.length - counter;i++)
+        {
+            setTimeout(()=>{
+                let val1 = parseInt(canvasChildren[i].textContent);
+                let val2 = parseInt(canvasChildren[(i+1)].textContent);
+                canvasChildren[i].style.background = spanCmpColor;
+                canvasChildren[i+1].style.background = spanCmpColor;
+                if(val1>val2)
+                {
+                    let temp = val1;
+                    val1 = val2;
+                    val2 = temp;
+                }
+                if(val1 > minElemHeight)
+                {
+                    canvasChildren[i].style.height = (minHeight+(val1-minElemHeight))+"px";
+                }
+                else
+                {
+                    canvasChildren[i].style.height = 0;
+                }
+                if(val2 > minElemHeight)
+                {
+                    canvasChildren[i+1].style.height = (minHeight+(val2-minElemHeight))+"px";
+                }
+                else
+                {
+                    canvasChildren[i].style.height = 0;
+                }
+                canvasChildren[i].textContent = val1;
+                canvasChildren[i+1].textContent = val2;
+            },time+=700);
+            setTimeout(()=>{
+                canvasChildren[i].style.backgroundColor = spanColor;
+                canvasChildren[i+1].style.backgroundColor = spanColor;
+            },time+=700);
+        }
+        counter++;
+    }
 });
