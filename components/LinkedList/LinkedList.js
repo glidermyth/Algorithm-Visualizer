995fc966686544632fe195d43cb9452b1a57211a
Glider Myth <glidermyth@gmail.com>
2022-05-16 22:32:52 +0530

    Making some improvements in LinkedList component and adding generate random and flush buttons.

--- components/LinkedList/LinkedList.js
+++ components/LinkedList/LinkedList.js
@@ -6,6 +6,8 @@ const insertAtTail = document.querySelector(".insertAtTail");
 const deleteFromTail = document.querySelector(".deleteFromTail");
 const front = document.querySelector(".front");
 const back = document.querySelector(".back");
+const generateRandom = document.querySelector(".generate-random");
+const flush = document.querySelector(".flush");
 const inputData = document.querySelector("#data_field");
 
 insertAtTail.addEventListener("click",(event)=>{
@@ -139,7 +141,7 @@ deleteFromTail.addEventListener("click",(event)=>{
     else if(linkedList.children.length == 1)
     {
         let p = document.createElement("p");
-        p.innerText = `${linkedList.firstElementChild.textContent} deleted successfully.`;
+        p.innerText = `${linkedList.lastElementChild.textContent} deleted successfully.`;
         display.append(p);
         linkedList.firstElementChild.remove();
         setTimeout(()=>{
@@ -149,7 +151,7 @@ deleteFromTail.addEventListener("click",(event)=>{
     else
     {
         let p = document.createElement("p");
-        p.innerText = `${linkedList.firstElementChild.textContent} deleted successfully.`;
+        p.innerText = `${linkedList.lastElementChild.textContent} deleted successfully.`;
         display.append(p);
         linkedList.lastElementChild.remove();
         linkedList.lastElementChild.remove();
@@ -194,6 +196,64 @@ back.addEventListener("click",(event)=>{
     {
         let p = document.createElement("p");
         p.innerText = `${linkedList.lastElementChild.textContent} is the front element.`;
+        display.append(p);
+        setTimeout(()=>{
+            display.lastElementChild.remove();
+        },2000);
+    }
+});
+
+generateRandom.addEventListener("click",(event)=>{
+    
+    let linkedListElemCount = linkedList.children.length;
+    for(let i = 0;i<linkedListElemCount;i++)
+    {
+        linkedList.firstElementChild.remove();
+    }
+    for(let i=0;i<13;i++)
+    {
+        if(linkedList.children.length == 0)
+        {
+            let span = document.createElement("span");
+            span.innerText = Math.ceil(Math.random() * (i+11));
+            linkedList.append(span);
+        }
+        else
+        {
+            let span = document.createElement("span");
+            span.innerText = Math.ceil(Math.random() * (i+11));
+            let img = document.createElement("img");
+            img.src = "../../Images/arrow.png";
+            linkedList.append(img);
+            linkedList.append(span);
+        }
+    }
+    let p1 = document.createElement("p");
+    p1.innerText = `Data generated successfully😉`;
+    display.appendChild(p1);
+    setTimeout(()=>{
+        display.lastElementChild.remove();
+    },2000);
+});
+
+flush.addEventListener("click",(event)=>{
+    let linkedListElemCount = linkedList.children.length;
+    if(linkedListElemCount == 0)
+    {
+        let p = document.createElement("p");
+        p.innerText = `Stack has no element🙄🙄`;
+        display.append(p);
+        setTimeout(()=>{
+            display.lastElementChild.remove();
+        },2000);
+    }
+    else{
+        for(let i=0;i<linkedListElemCount;i++)
+        {
+            linkedList.firstElementChild.remove();
+        }
+        let p = document.createElement("p");
+        p.innerText = `Stack flushed successfully😊😊😊`;
         display.append(p);
         setTimeout(()=>{
             display.lastElementChild.remove();
